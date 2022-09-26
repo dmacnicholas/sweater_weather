758102b72a4ddf9aa9fc735203a394beeec10e83
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-26 10:19:25 -0500

    Add current forecat poros

---
+++ app/poros/current_forecast.rb
@@ -0,0 +1,16 @@
+class CurrentForecast
+  def self.data(attributes)
+    {
+      datetime: Time.at(attributes[:dt]),
+      sunrise: Time.at(attributes[:sunrise]),
+      sunset: Time.at(attributes[:sunset]),
+      temperature: attributes[:temp],
+      feels_like: attributes[:feels_like],
+      humidity: attributes[:humidity],
+      uvi: attributes[:uvi],
+      visibility: attributes[:visibility],
+      conditions: attributes[:weather].first[:description],
+      icon: attributes[:weather].first[:icon]
+    }
+  end
+end
