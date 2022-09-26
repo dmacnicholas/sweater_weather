9ffd742d0921f2c857f36a46cf5da837d12efd69
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-26 10:51:16 -0500

    Add library service and test

---
+++ spec/services/library_service_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe LibraryService do
+  it 'returns book data', :vcr do
+      location = "Nashville, TN"
+      quantity = 5
+
+      search = LibraryService.new
+      results = search.book_search(location, quantity)
+
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:numFound)
+      expect(results[:numFound]).to be_a(Integer)
+
+      expect(results).to have_key(:docs)
+      expect(results[:docs]).to be_a(Array)
+      expect(results[:docs].count).to eq(quantity)
+
+      book = results[:docs].first
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a(String)
+
+      expect(book).to have_key(:isbn)
+      expect(book[:isbn]).to be_a(Array)
+      book[:isbn].each do |num|
+      expect(num).to be_a(String)
+      end
+
+      expect(book).to have_key(:publisher)
+      expect(book[:publisher]).to be_a(Array)
+      book[:publisher].each do |pub|
+      expect(pub).to be_a(String)
+    end
+  end
+end  
