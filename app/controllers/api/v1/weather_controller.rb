d33a707056a9f0c33d818369064ea3b6a9dbeaba
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-26 10:12:14 -0500

    Add weather controller and index method

---
+++ app/controllers/api/v1/weather_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::WeatherController < ApplicationController
+   def index
+       location = LocationFacade.create_location(params[:location])
+
+       forecast = WeatherFacade.create_forecast(location.latitude, location.longitude)
+
+       render json: ForecastSerializer.new(forecast)
+   end
+end
