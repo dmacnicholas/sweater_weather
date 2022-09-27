aa9ce026b38bfd38bbeff3664418319a4a4a845c
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-27 14:34:41 -0500

    Bcrypt gem

--- app/facades/book_forecast_facade.rb
+++
@@ -1,8 +0,0 @@
-# class BookForecastFacade
-#
-#   def self.get_forecast(location)
-#     location = LocationFacade.create_location(location)
-#     forecast = WeatherFacade.create_forecast(latitude, longitude)
-#   end
-#
-# end
