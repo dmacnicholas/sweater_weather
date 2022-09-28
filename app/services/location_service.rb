76180efb18165409342264e98d090b002f30c297
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-28 11:35:12 -0500

    Add directions_search method

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,12 +1,17 @@
 class LocationService
   def self.conn
-      Faraday.new("https://www.mapquestapi.com/geocoding/v1/address") do |faraday|
+      Faraday.new("https://www.mapquestapi.com") do |faraday|
           faraday.params['key'] = ENV['map_quest_key']
       end
   end
 
   def self.location_search(location)
-      response = conn.get("?location=#{location}")
+      response = conn.get("/geocoding/v1/address?location=#{location}")
+      json = JSON.parse(response.body, symbolize_names: true) if response.status == 200
+  end
+
+  def self.directions_search(origin, destination)
+      response = conn.get("/directions/v2/route?from=#{origin}&to=#{destination}")
       json = JSON.parse(response.body, symbolize_names: true) if response.status == 200
   end
 end
