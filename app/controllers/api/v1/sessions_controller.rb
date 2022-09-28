0c46aec9e51c8a306595a498fba1824ca613ca85
Drew MacNicholas <dmacnicholas@gmail.com>
2022-09-27 19:07:43 -0500

    Update create method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email].downcase)
 
-    if user.nil?
-      render(json: { message: "User does not exist. Please register!" }, status: :unauthorized)
-    elsif user.authenticate(params[:password])
+    if !user.nil? && user.authenticate(params[:password])
       render(json: UsersSerializer.response_body(user), status: :ok)
     else
       render(json: { message: "User email or password is invalid." }, status: :unauthorized)
